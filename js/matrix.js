9f34666e76cbf4e745456f049b803b0a55272bff
mdarman.alam <mdarman.alam@gmail.com>
2017-07-19 15:10:33 +0530

    added some link in navigation

--- js/matrix.js
+++ js/matrix.js
@@ -163,26 +163,3 @@ $(document).ready(function(){
 	});
 	
 });
-
-// Hide View Source //
-document.onkeydown = function(e) {
-    if(e.keyCode == 123) {
-     return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-     return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-     return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-     return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
-     return false;
-    } 
-	if(e.keyCode == 'f12'.charCodeAt(0)){
-     return false;
-    } 	
- }
-
